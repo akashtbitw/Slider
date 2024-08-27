bd7af8373b98ceb841d3e7932972a6268b6936d2
Akash Saha <sahaaakash201@gmail.com>
2024-08-27 10:20:53 +0530

    refactored code

--- index.js
+++ index.js
@@ -5,27 +5,31 @@ const images = document.querySelectorAll(".image")
 const length = images.length
 let slideNumber = 1
 
+const nextSlide = () => {
+    slider.style.transform = `translateX(-${slideNumber * 800}px)`
+    slideNumber++
+}
+
+const prevSlide = () => {
+    slider.style.transform = `translateX(-${(slideNumber - 2) * 800}px)`
+    slideNumber--
+}
+
+const firstSlide = () => {
+    slider.style.transform = `translateX(0px)`
+    slideNumber = 1
+}
+
+const lastSlide = () => {
+    slider.style.transform = `translateX(-${(length - 1) * 800}px)`
+    slideNumber = length
+}
+
 right.addEventListener("click", () => {
-    if(slideNumber < length) {
-        slider.style.transform = `translateX(-${slideNumber * 800}px)`
-        slideNumber++
-    }
-    else {
-        slider.style.transform = `translateX(0px)`
-        slideNumber = 1
-    }
-    
+    slideNumber < length ? nextSlide() : firstSlide()
 })
 
 left.addEventListener("click", () => {
-    if(slideNumber > 1) {
-        slider.style.transform = `translateX(-${(slideNumber - 2) * 800}px)`
-        slideNumber--
-    } 
-    else {
-        slider.style.transform = `translateX(-${(length - 1) * 800}px)`
-        slideNumber = length
-    }
-    
+    slideNumber > 1 ? prevSlide() : lastSlide()
 })
 
