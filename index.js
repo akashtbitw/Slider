56bd9f4505a6659bfdb7f7e6d58cda065e0560f0
Akash Saha <sahaaakash201@gmail.com>
2024-08-27 11:59:04 +0530

    made arrow and dot button synchronous

--- index.js
+++ index.js
@@ -6,6 +6,28 @@ const bottom = document.querySelector(".bottom")
 const length = images.length
 let slideNumber = 1
 
+for (let i = 0; i < length; i++) {
+    const div = document.createElement("div")
+    div.className = "button"
+    bottom.appendChild(div)
+}
+const resetBg = () => {
+    buttons.forEach((button) => {
+        button.style.backgroundColor = "transparent"
+    })
+}
+
+const buttons = document.querySelectorAll(".button")
+buttons[0].style.backgroundColor = `rgb(246, 20, 20)`
+
+buttons.forEach((button, i) => {
+    button.addEventListener("click", () => {
+        resetBg()
+        slider.style.transform = `translateX(-${i * 800}px)`
+        buttons[i].style.backgroundColor = `rgb(246, 20, 20)`
+    })
+})
+
 const nextSlide = () => {
     slider.style.transform = `translateX(-${slideNumber * 800}px)`
     slideNumber++
@@ -26,32 +48,22 @@ const lastSlide = () => {
     slideNumber = length
 }
 
+const changeColor = () => {
+    resetBg()
+    buttons[slideNumber - 1].style.backgroundColor = `rgb(246, 20, 20)`
+}
+
+
 right.addEventListener("click", () => {
     slideNumber < length ? nextSlide() : firstSlide()
+    changeColor()
 })
 
 left.addEventListener("click", () => {
     slideNumber > 1 ? prevSlide() : lastSlide()
+    changeColor()
 })
 
-for (let i = 0; i < length; i++) {
-    const div = document.createElement("div")
-    div.className = "button"
-    bottom.appendChild(div)
-}
-const resetBg = () => {
-    buttons.forEach((button) => {
-        button.style.backgroundColor = "transparent"
-    })
-}
-const buttons = document.querySelectorAll(".button")
-buttons[0].style.backgroundColor = `rgb(246, 20, 20)`
 
-buttons.forEach((button, i) => {
-    button.addEventListener("click", () => {
-        resetBg()
-        slider.style.transform = `translateX(-${i * 800}px)`
-        buttons[i].style.backgroundColor = `rgb(246, 20, 20)`
-    })
-})
+
 
